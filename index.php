8b88904a06445e5e662c9a974ec0a46c322f0495
adam-kollberg <adam@web-it.nu>
2021-03-01 23:40:58 +0100

    added single posts

--- index.php
+++ index.php
@@ -1,43 +1,68 @@
-<?php include("database/dbconnection.php"); ?>
-<?php include("includes/header.php"); ?>
+<?php 
+include("database/dbconnection.php");
+include("path.php");
+?>
+
+
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
+
+  <!-- Custom Styling -->
+  <link rel="stylesheet" href="assets/style2.css">
+
+  <title>Blog</title>
+</head>
+
+<?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+
 
 
 <body>
 
-    <h2>Blog-5<h2>
 
 
-<a href="<?php echo 'login.php'; ?> " > Login</a>
-<br>
-
-<a href="<?php echo 'register.php'; ?>">Registration</a>
-
-
-<a href="<?php echo 'post.php'; ?>">Post</a>
-
-<?php $stm = $pdo->query("SELECT * FROM posts ");?>
+<?php $stm = $pdo->query("SELECT * FROM posts");?>
 <div class="page-wrapper">
+
+<div class="hero">
+<h1 class="hero-heading"> WELCOME TO MILLHOUSE, THE BLOG</h1>
+<p class = hero-subheading>We are looking for better customer relations
+please feel free to comment on our blog posts!
+</p>
+
+</div>
 
 
 <!-- Post Slider -->
    
-      <h1 class="slider-title">Trending Posts</h1>
+      <h1 class="slider-title">New Posts</h1>
     
       <?php while ($row = $stm->fetch()) { ?>
       <div class="post-wrapper">
       
         <div class="post">
-       
+        
           <img src="<?php echo $row['image']?>  " alt="" class="slider-image">
           <div class="post-info">
-            <h4><a href="single.html"><?php echo $row['title']?></a></h4>
+            <h3><?php echo $row['title'];?></h3>
             
-            <button class ="btn">Read more</button>
+            <a href="single-post.php?id=<?php echo $row['id']; ?>">Read More</a>
             
           </div>
+          
         </div>
+        
         <?php } ?> 
-
 
         
         
@@ -46,11 +71,18 @@
       </div>
 
     </div>
+      </div>
+
 
 
     
 
 
 
-<?php include("includes/footer.php")?>
+    <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+
+
+</body>
+
+</html>
 
---
+++ single-post.php
@@ -0,0 +1,61 @@
+<?php include("path.php"); 
+include("database/dbconnection.php");
+?>
+
+
+<?php 
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM posts WHERE id=$id";
+    $stm = $pdo->prepare($sql);
+    $stm->execute();
+    $result = $stm->fetch();
+
+
+
+  }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
+
+  <!-- Custom Styling -->
+  <link rel="stylesheet" href="assets/style2.css">
+
+  <title><?php echo $result['title']; ?> | Millhouse</title>
+</head>
+<body>
+
+<?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+<div class="single-content">
+
+<div class="single-wrapper">
+        <div class="single">
+        <img src="<?php echo BASE_URL . "/". $result['image']?>" alt="" width="500" height="500">
+        <h1 class="post-title"><?php echo $result['title']; ?></h1>
+
+          <div class="post-content">
+            <?php echo ($result['body']); ?>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+      <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+</body>
+</html>
